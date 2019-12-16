b485eadb9e897a91f5bb6bcecbe6b33873488db6
Al - Mustanjid <mustanjid.se@gmail.com>
2019-12-16 11:25:51 +0600

    Update Access modifier file

--- Access_Modifier.php
+++ Access_Modifier.php
@@ -15,14 +15,31 @@ class MyClass{
     //declaring $string name as protected property 
     protected $string = 'Access Modifier';
     
-    function PropertyPrint(){
+    public function PropertyPrint(){
         echo $this->font_size.'<br/>';
         echo $this->font_color.'<br/>';
         echo $this->string.'<br/>';
     }
+    
+    private function my_private_method(){
+        echo "This is a private method";
+    }
+    
+    protected function my_protected_method(){
+        echo 'This is my protected method';
+    }
+    
+    public function test(){
+        echo $this->my_private_method();
+        echo $this->my_protected_method();
+    }
 }
+
 $obj = new MyClass();
 echo $obj->font_size.'<br/>'; //display 18px
-@$obj->font_color; //it creates fatal error
-@$obj->string; //it creates fatal error
+$obj->test(); //it prints the methods inside the function
+//@$obj->font_color; //it creates fatal error
+//@$obj->string; //it creates fatal error
 $obj->PropertyPrint();
+$obj->my_private_method(); //it creates fatal error
+$obj->my_protected_method(); //it creates fatal error
