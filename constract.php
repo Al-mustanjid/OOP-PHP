f11bb0131226c3f49a24a2dc1ac01d5a297fa4e7
unknown <mustanji.se@gmail.com>
2019-12-24 13:24:45 +0600

    new file added

---
+++ constract.php
@@ -0,0 +1,25 @@
+<?php
+/*onstructor: constructor is a codeblock which executes automatically during object creation
+it is written by double underscore __ before constructor name
+*/
+
+class MyClass{
+    private $name;
+    private $age;
+    private $section;
+
+    public function __construct($name, $age, $section){
+        $this->name = $name;
+        $this->age = $age;
+        $this->section = $section;
+    }
+
+    public function infoPrint(){
+        echo $this->name.'<br/>';
+        echo $this->age.'<br/>';
+        echo $this->section.'<br/>';
+    }
+}
+
+$object1 = new MyClass("Arif","24","H");
+$object1->infoPrint();
